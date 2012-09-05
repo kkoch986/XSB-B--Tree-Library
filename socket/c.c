f6c7c2a6c0d1badb247d0dc148b3b58db2d5d42f
ken <ken@kens-ubuntu.(none)>
2012-09-04 22:58:31 -0400

    working on socket stuff

--- socket/c.c
+++ socket/c.c
@@ -9,6 +9,9 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
+const unsigned int OP_QUERY = 0;
+char* build_op_query(char *query);
+
 int main()
 {
 	char *msg_body;
@@ -34,30 +37,59 @@ int main()
 	if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
 		printf("ERROR 2");
 
-	msg_body = "Hello";
-	msg_body_len = strlen(msg_body); 
+	msg_body = "a(b,c)";
+	char *message = build_op_query(msg_body);
 
-	char message[msg_body_len + 5];
-	
-	network_encoded_len = (unsigned int) htonl((unsigned long int) msg_body_len); 
-
-	memcpy((void *) message, (void *) &network_encoded_len, 4); 
-	strncpy(message+4, msg_body, msg_body_len);
-
-	n = send(sockfd, message, strlen(message + 4), 0);
+	printf("sending %s", message+4);
+	n = send(sockfd, message, strlen(message), 0);
 	printf("sent %s", message+4);
 
 	if(n < 0)
 		printf("Error writing to socket\n");
 
-	bzero(buffer, 256);
+	// bzero(buffer, 256);
 
-	n = read(sockfd, buffer, 255);
+	// n = read(sockfd, buffer, 255);
 
-	printf("RECV: %s\n", buffer);
-
+	// printf("RECV: %s\n", buffer);
 
 	close(sockfd);
 
 	return 0;
+}
+
+// OP_QUERY Packet:
+// 4 Bytes Total Message Length (unsigned int) (message[0 - 3])
+// 4 Byte OP_CODE (unsigned int) (message[4 - 7])
+// 4 Bytes Query String Length (unsigned int) (message[8 - 11])
+// <Query String Length> Bytes Query String
+//
+// Returned value is allocated using malloc
+// so it should be freed when done
+char* build_op_query(char *query)
+{
+	// the query returned will be the op code
+	unsigned int query_length = strlen(query);
+
+	// set aside some space for the message
+	char *message = malloc(sizeof(char) * query_length + 16);
+
+	// start with the network encoded length
+	unsigned int network_encoded_len = (unsigned int) htonl((unsigned long int) (4 + query_length)); 
+	memcpy((void *) message, (void *) &network_encoded_len, 4); 
+	
+	// now the op code
+	// memcpy((void *) message+4, (void *) &OP_QUERY, 2); 
+	strncpy(message+4, itoa(OP_QUERY), 2); 
+
+	// now the query length
+	// memcpy((void *) message+6, (void *) &query_length, 2); 
+	strncpy(message+6, itoa(query_length), 2); 
+
+	// now the query string
+	strcpy(message+8, query);
+
+	printf("message: %s\n", message + 4);
+
+	return message;
 }
